950f0238e34abca76aadfa4d725840d43623562b
Itskarajov15 <asen__7@abv.bg>
2022-07-14 19:38:43 +0300

    Changed file directories

---
+++ HotelBooking/Data/HotelBookingDbContext.cs
@@ -0,0 +1,56 @@
+﻿namespace HotelBooking.Data
+{
+    using HotelBooking.Data.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class HotelBookingDbContext : IdentityDbContext
+    {
+        public HotelBookingDbContext(DbContextOptions<HotelBookingDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<HotelImage> HotelImages { get; set; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<RoomType> RoomTypes { get; set; }
+
+        public DbSet<RoomImage> RoomImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<City>()
+                .HasOne(x => x.Country)
+                .WithMany(x => x.Cities)
+                .HasForeignKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Hotel>()
+                .HasOne(x => x.City)
+                .WithMany(x => x.Hotels)
+                .HasForeignKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Hotel>()
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Hotels)
+                .HasForeignKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
