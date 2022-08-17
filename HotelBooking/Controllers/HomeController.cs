bbb4c8c2b70be3b570ee303fe9c6b832cf4b8683
Itskarajov15 <asen__7@abv.bg>
2022-08-17 09:49:30 +0300

    sdhgdfg

--- HotelBooking/Controllers/HomeController.cs
+++ HotelBooking/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using HotelBooking.Models;
+﻿using HotelBooking.Core.Contracts;
+using HotelBooking.Core.Models.Rooms;
+using HotelBooking.Core.Services;
+using HotelBooking.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +9,39 @@ namespace HotelBooking.Controllers
 {
     public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IHotelService hotelService;
+        private readonly IService service;
+        private readonly IReservationService reservationService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IHotelService hotelService,
+            IService service,
+            IReservationService reservationService)
         {
-            _logger = logger;
+            this.hotelService = hotelService;
+            this.service = service;
+            this.reservationService = reservationService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() => this.View(new FilterRoomsViewModel
         {
-            return View();
+            Cities = this.hotelService.GetCityNames()
+        });
+
+        [HttpPost]
+        public IActionResult Index(FilterRoomsViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            if (!service.ValidateDates(model.StartDate, model.EndDate))
+            {
+                ModelState.AddModelError(String.Empty, "Invalid dates");
+                return this.View();
+            }
+
+            return Ok(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
